0a0f897a0f2d7ebb26dfa86d6c24a625310036e9
zocomputer <help@zocomputer.com>
2026-04-25 19:59:51 +0000

    Initial: Vincent Kan personal website

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Vincent Kan';
+export const SITE_DESCRIPTION = '個人部落格 & Project Showcase — 科技、UFO、日常';
