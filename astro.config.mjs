a878ec0626544a9c2f59a604e9a1807921104cca
vincentsckan <vincentsc.kan@gmail.com>
2026-04-30 22:59:17 +0800

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vincentsckan.github.io',
-  base: '/vincent-kan-site',
+  site: 'https://www.disclosurehk.com',
+  base: '/',
 
   integrations: [mdx(), sitemap(), react()],
   fonts: [
